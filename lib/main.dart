648e3d73a788626a3f0de073da60715797fc08fa
bohairi <ahmadalbouhire@gmail.com>
2025-11-09 15:36:23 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_all_applications/quis_app/first_page.dart';
 import 'package:flutter_all_applications/four/icon_page2.dart';
 import 'package:flutter_all_applications/seven/button_types.dart';
 import 'package:flutter_all_applications/seven/page1.dart';
+import 'package:flutter_all_applications/six/home_page.dart';
+import 'package:flutter_all_applications/stack_UI_task/home_page_stack.dart';
 import 'package:flutter_all_applications/views/grid_view_adv.dart';
 import 'package:flutter_all_applications/views/grid_view_class.dart';
 import 'package:flutter_all_applications/views/leason_list_view.dart';
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StackClass(),
+      home: HomePageStack(),
     );
   
   }
---
+++ lib/stack_UI_task/home_page_stack.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class HomePageStack extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 110, 165, 209),
+      appBar: AppBar(
+        backgroundColor:const Color.fromARGB(255, 79, 5, 119) ,
+        title: Center(
+          child: Text("Sign In",
+          style: TextStyle(
+            color: Colors.white
+          ),),
+        ),
+      ),
+      body: SafeArea(child: 
+      SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.09,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+            Icon(Icons.person_2_sharp),
+            Text("Shelton Cooper")]),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                customCircle(Colors.deepPurple),
+                customCircle(Colors.grey),
+                customCircle(Colors.grey),
+                customCircle(Colors.grey),
+              ],
+            ),
+          ),
+          Card(
+              margin: EdgeInsets.only(top: 15),
+            color: Colors.white,
+            child: Container(
+              decoration: BoxDecoration(
+              ),
+              height:MediaQuery.of(context).size.height * 0.6,
+              width: MediaQuery.of(context).size.height * 0.5,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  ),
+                  itemCount: 9,
+                   itemBuilder: (context,index){
+                return customNumber(index+1);
+              }),
+            ),
+          )
+          ]
+        ),
+      )),
+    );
+  }
+}
+Widget customCircle(Color c){
+  return Container(
+    height: 20,
+    width: 30,
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: c
+    ),
+  );
+}
+Widget customNumber(int data){
+  return Container(
+    decoration: BoxDecoration(
+    shape: BoxShape.circle
+    ),
+    height: 15,
+    width: 15,
+    child: Center(child: Text("$data",
+    style: TextStyle(
+      color: Colors.grey,
+      fontSize: 20
+    ),)),
+  );
+}
